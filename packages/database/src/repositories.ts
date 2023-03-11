9ebd0f5d81af50091a678672045e74eadb78a747
stLmpp <gui.stlmpp@gmail.com>
2023-03-11 11:41:03 -0300

    working in the prisma schema

--- packages/database/src/repositories.ts
+++ packages/database/src/repositories.ts
@@ -11,8 +11,36 @@ export class GameRepository extends createRepository('game') {}
 @Repository()
 export class MiniGameRepository extends createRepository('miniGame') {}
 @Repository()
+export class InputTypeRepository extends createRepository('inputType') {}
+@Repository()
+export class ModeRepository extends createRepository('mode') {}
+@Repository()
+export class StageRepository extends createRepository('stage') {}
+@Repository()
+export class CharacterRepository extends createRepository('character') {}
+@Repository()
+export class CharacterCostumeRepository extends createRepository('characterCostume') {}
+@Repository()
 export class GameMiniGameRepository extends createRepository('gameMiniGame') {}
 @Repository()
 export class PlatformGameMiniGameRepository extends createRepository('platformGameMiniGame') {}
+@Repository()
+export class PlatformGameMiniGameModeRepository extends createRepository('platformGameMiniGameMode') {}
+@Repository()
+export class PlatformGameMiniGameModeCharacterCostumeRepository extends createRepository('platformGameMiniGameModeCharacterCostume') {}
+@Repository()
+export class PlatformGameMiniGameModeStageRepository extends createRepository('platformGameMiniGameModeStage') {}
+@Repository()
+export class PlatformInputTypeRepository extends createRepository('platformInputType') {}
+@Repository()
+export class RegionRepository extends createRepository('region') {}
+@Repository()
+export class PlayerRepository extends createRepository('player') {}
+@Repository()
+export class ScoreRepository extends createRepository('score') {}
+@Repository()
+export class ScorePlayerRepository extends createRepository('scorePlayer') {}
+@Repository()
+export class ScoreWorldRecordRepository extends createRepository('scoreWorldRecord') {}
 
 MAIN_INJECTOR.register([new ValueProvider(PrismaClient, new PrismaClient())]);
